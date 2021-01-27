1ab6ee012cc25d71dc41f6384f85f81b118eea61
Dave Powell <evadllewop@gmail.com>
2021-01-26 20:41:20 -0600

    updated files for heroku

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,13 +4,13 @@ const path = require("path");
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
   app.get("/login", (req, res) => {
