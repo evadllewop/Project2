34bbcc4c54bceacb42009e9be481b846c8264051
Jay Valdez <jj.valdez09@gmail.com>
2021-01-26 22:05:22 -0600

    made the sign up form work

--- models/user.js
+++ models/user.js
@@ -15,7 +15,7 @@ module.exports = function(sequelize, DataTypes) {
     // The password cannot be null
     password: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false 
     },
     name: {
       type: DataTypes.STRING,
