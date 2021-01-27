03cdeefcf6b0382aa62fd7b8c3e876f66867ecbf
Dave Powell <evadllewop@gmail.com>
2021-01-27 14:24:05 -0600

    updating calendar

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/createProfile");
     }
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
